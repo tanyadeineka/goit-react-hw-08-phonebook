f7924435b6edd6ca3567d15ad4ec4c6af0680179
Tetiana Deineka <tata4ka666@i.ua>
2023-01-12 13:23:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { getUser } from 'redux/operations';
+import s from '../components/App.module.css'
+import { AppBar } from './AppBar/AppBar';
+import { Register } from './Register/Register';
+import { Login } from './Login/Login';
+import { Contacts } from './Contacts/Contacts';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
+
+    return (
+      <div>
+        <AppBar />
+        <Routes>
+          <Route path="/" element={<div className={s.title}>Welcome to our phonebook!</div>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+        <ToastContainer autoClose={2000} />
+      </div>
+    );
+  
+  
 };
