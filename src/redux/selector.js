5d5140bacf55f27dc9afd04aca08c610bdf00f3d
Tetiana Deineka <tata4ka666@i.ua>
2023-01-12 13:24:56 +0200

    Create selector.js

---
+++ src/redux/selector.js
@@ -0,0 +1,11 @@
+export const selectIsAuth = state => state.auth.isAuth
+
+export const selectGetEmail = state => state.auth.email
+
+export const selectAuthIsLoading = state => state.auth.isLoading
+
+export const selectItemsIsLoading = state => state.items.isLoading
+
+export const selectGetItemsContacts = state => state.items.contacts
+
+export const selectGetFilterValue = state => state.filter
